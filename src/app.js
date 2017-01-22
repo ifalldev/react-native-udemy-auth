24f8ff0f33c057af6a5da68999aedcc7b9660bb9
ifalldev <cbatista.93@gmail.com>
2017-01-22 14:25:01 -0200

    button correction

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDF2viDL0vVwd_KGGtYX029czFyMh9b0eE',
@@ -13,13 +15,42 @@ class App extends Component {
       storageBucket: 'auth-native-test.appspot.com',
       messagingSenderId: '793068243300',
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+    case true:
+      return (
+        <CardSection>
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        </CardSection>
+      );
+    case false:
+      return <LoginForm />;
+    default:
+      return (
+        <CardSection>
+          <Spinner size="large" />
+        </CardSection>
+      );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
