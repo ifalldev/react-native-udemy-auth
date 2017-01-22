6e69210e6504bdb6fdcd534ac30637cb45bb7a3e
ifalldev <cbatista.93@gmail.com>
2017-01-22 05:00:46 -0200

    putting spinner component on login form

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -3,7 +3,7 @@ import { View, ActivityIndicator } from 'react-native';
 
 const Spinner = ({ size }) => {
   return (
-    <View style={styles.spinnerStyles}>
+    <View style={styles.spinnerStyle}>
       <ActivityIndicator size={size || 'large'} />
     </View>
   );
