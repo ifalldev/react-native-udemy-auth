a0374d4ab929591f7c9f5049461025cf3b5b70c0
ifalldev <cbatista.93@gmail.com>
2017-01-22 02:43:59 -0200

    firebase config

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDF2viDL0vVwd_KGGtYX029czFyMh9b0eE',
+      authDomain: 'auth-native-test.firebaseapp.com',
+      databaseURL: 'https://auth-native-test.firebaseio.com',
+      storageBucket: 'auth-native-test.appspot.com',
+      messagingSenderId: '793068243300',
+    });
+  }
+
   render() {
     return (
       <View>
